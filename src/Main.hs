0cae888b6b0c7b90beff2a7a1a8189b39ad73a8f
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-06-29 14:41:56 +0530

    Moving to an RWST monad for sequencing computations

--- src/Main.hs
+++ src/Main.hs
@@ -1,45 +1,104 @@
 module Main where
 
 import           Numeric (showHex)
-import           System.Random.MWC(initialize, uniform, GenIO)
+import qualified System.Random.MWC as MWC
 import           Control.Monad (replicateM)
 import qualified Data.Vector.Unboxed as DV
 import qualified Data.Map.Strict as Map
 import           Data.Word(Word64)
 import           Data.List (sortBy, elemIndex, findIndex, length, nub)
 import           Data.Tuple (swap)
-import           Data.Ord (comparing)
 import           Data.Bits (xor)
-import           Data.ByteString.Builder
 import qualified Data.Graph.Generators as GG
 import           Data.Graph.Generators.Random.WattsStrogatz (wattsStrogatzGraph)
 import qualified Data.Graph.Inductive.Graph as GIG
 import qualified Data.Graph.Inductive.Basic as GIB
 import           Data.Graph.Inductive.PatriciaTree (Gr)
+import           Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
 import qualified Data.Map.Strict as Map
+import           Control.Monad.RWS
 
-type NodeAddress = Word64
-type AddressDistance = Word64
+type GraphOrder = Int
+type NumRingNeighbors = Int
+type RewiringProb = Double
+
+data WattsStrogatzGraph = WattsStrogatz {
+  go    :: GraphOrder,
+  nrn   :: NumRingNeighbors,
+  rp    :: RewiringProb
+}
+
+data GraphType = WattsStrogatzGraph | MeshGraph -- Mesh graph TBD
+
+type ScanRadius = Int
+type NumBeacons = Int
+type NumCandidateRoutes = Int
+type NumQueriedNodes = Int
+
+data FlareRouting = Flare {
+  sr  :: ScanRadius,
+  nb  :: NumBeacons,
+  ncr :: NumCandidateRoutes,
+  nqn :: NumQueriedNodes
+}
+
+data RoutingType = FlareRouting | GossipRouting -- Gossip routing TBD
+
+data LNetworkConfig = LNetworkConfig {
+  graphType     :: GraphType,
+  routingType   :: RoutingType
+}
+
+type NumHelloMessages = Int
+type NumBeaconRequests = Int
+
+data LNetworkStatistics = LNetworkStatistics {
+  nhm :: NumHelloMessages,
+  nbr :: NumBeaconRequests
+}
+
+initialStats = LNetworkStatistics {
+  nhm = 0,
+  nbr = 0
+}
+
+instance Monoid LNetworkStatistics where
+  mempty = initialStats
+  s1 `mappend` s2 = LNetworkStatistics {
+    nhm = nhm s1 + nhm s2,
+    nbr = nbr s1 + nbr s2
+  }
+
+oneHelloMessageSent = initialStats { nhm = 1}
+oneBeaconRequestSent = initialStats { nbr = 1}
+
+type Channel = GIG.Edge -- A channel is a ordered pair of vertices
 type Satoshi = Int
-
--- A channel is a ordered pair of vertices
-type Channel = GIG.Edge
-
-
 
 -- Node state has the routing table which is just a
 -- collection of directed edges with associated capacity.
 -- Later node state can have up/down information
-data NodeState = NS {
+data NodeState = NodeSt {
   rtable  :: Map.Map Channel Satoshi
 }
+
+type ChannelCapacityMap = Map.Map Channel Satoshi
+type NodeStateMap = IntMap NodeState
+
+data LNetworkState = LNetworkState {
+  networkGraph      :: Gr NodeAddress (),
+  channelCapacities :: ChannelCapacityMap,
+  nodeStateMap      :: NodeStateMap,
+  seedForRNG        :: Int
+}
+
+type Event = RWST LNetworkConfig LNetworkStatistics LNetworkState IO ()
 
 -- Watts-Strogatz graph generation parameters
 numVertices = 2000
 numRingNeighbors = 4
 probRewiring = 0.3 :: Double
-seedForRNG = 212
 
 -- Flare routing parameters
 numBeacons = 2
@@ -48,8 +107,11 @@ numQueriedNodes = 10
 scanRadius = 2
 oneBTC = 100000000 :: Satoshi
 
-genAddress :: GenIO -> IO NodeAddress
-genAddress = uniform
+type NodeAddress = Word64
+type AddressDistance = Word64
+
+genAddress :: MWC.GenIO -> IO NodeAddress
+genAddress = MWC.uniform
 
 hexAddress :: NodeAddress -> String
 hexAddress addr = (++) "0x" $ showHex addr ""
@@ -57,27 +119,32 @@ hexAddress addr = (++) "0x" $ showHex addr ""
 dist :: NodeAddress -> NodeAddress -> AddressDistance
 dist = xor
 
-
 main :: IO ()
 main = do
+--  gen <- MWC.initialize (DV.singleton seedForRNG)
+--  -- Graph initialization
+--  wG <- wattsStrogatzGraph gen numVertices numRingNeighbors probRewiring
+--  addresses <- replicateM numVertices $ genAddress gen
 
-  -- Graph initialization
-  gen <- initialize (DV.singleton seedForRNG)
-  wG <- wattsStrogatzGraph gen numVertices numRingNeighbors probRewiring
-  addresses <- replicateM numVertices $ genAddress gen
+--  let vertexList = [0..(numVertices-1)]
+--      -- Label the vertices with their addresses
+--      labelledNodes = zip vertexList addresses
+--
+--      -- Add edges in the reverse direction to existing edges
+--      edgeList = GG.edges wG ++ (swap <$> GG.edges wG)
+--
+--      -- Label the edges with the unit (). We maintain the capacity
+--      -- of the edges in a Map for efficient updates.
+--      labelledEdges = (\e -> GIG.toLEdge e ()) <$> edgeList
+--
+--      -- Make a graph with labelled nodes and edges
+--      g = GIG.mkGraph labelledNodes labelledEdges :: Gr NodeAddress ()
+--
+--      -- Initialize all channels to have a capacity of 1 BTC
+--      netState = LNetworkState $ Map.fromList $ zip edgeList (repeat oneBTC)
+--
+--      -- Initialize the routing tables of all the nodes
+--      nodeStates = IntMap.fromList $ zip vertexList (repeat NodeSt { rtable = Map.empty })
 
-  let vertexList = [0..(numVertices-1)]
-      -- Label the vertices with their addresses
-      labelledNodes = zip vertexList addresses
-
-      -- Add edges in the reverse direction to existing edges
-      edgeList = GG.edges wG ++ (swap <$> GG.edges wG)
-
-      -- Label the edges with their capacity. All edge capacities
-      -- initialized to 1 BTC. Note that the edges are directed.
-      labelledEdges = (\e -> GIG.toLEdge e oneBTC) <$> edgeList
-
-      -- Make a graph with labelled nodes and edges
-      g = GIG.mkGraph labelledNodes labelledEdges :: Gr NodeAddress Satoshi
 
   putStrLn "Done"
